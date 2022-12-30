474f156e24e297c4b767c86212069726ad1c9585
cathyholloway <c.holloway@ucl.ac.uk>
2022-12-30 12:58:47 +0000

    Cleaned up site for fist share with team

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,7 +83,7 @@ const config = {
             items: [
               {
                 label: 'Introduction',
-                to: '/docs/Introduction',
+                to: '/docs/welcome',
               },
           
               {
